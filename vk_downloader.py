259a70726559b2bebe88d72d19b80370cb01d447
Zardimo <vasyasapeggin@rambler.ru>
2019-10-28 13:09:36 +0500

    detele function, add os.path.splitext, change functions name, change requirements.txt, add raise_for_status

--- vk_downloader.py
+++ vk_downloader.py
@@ -6,7 +6,7 @@ import webbrowser
 import random
 
 
-def get_upload_url(access_token, group_id):
+def get_comics_upload_url(access_token, group_id):
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     params = {
         'group_id' : group_id,
@@ -22,27 +22,25 @@ def dowload_image(image_name, image_url):
     path = os.path.join(os.getcwd(), 'vk_comics')
     os.makedirs('vk_comics', exist_ok=True)
     response = requests.get(image_url, verify=False)
+    response.raise_for_status()
     with open(os.path.join(path, image_name), 'wb') as image:
         image.write(response.content)
 
 
-def get_comment_and_name():
+def get_comics_comment_and_name():
     response = requests.get('https://xkcd.com/info.0.json')
+    response.raise_for_status()
     last_comics_num = response.json()['num']
     random_comics_num = random.randint(1, last_comics_num)
     comics_response = requests.get(
                         f'https://xkcd.com/{random_comics_num}/info.0.json')
+    comics_response.raise_for_status()
     comics = comics_response.json()
     comics_comment = comics['alt']
     comics_url = comics['img']
-    full_name_comics = f'{comics["title"]}.{get_extension(comics_url)}'
+    full_name_comics = f'{comics["title"]}.{os.path.splitext(comics_url)[1]}'
     dowload_image(full_name_comics, comics_url)
     return comics_comment, full_name_comics
-
-
-def get_extension(url):
-    last_path = url.split('.')[-1]
-    return last_path
 
 
 def get_download_image_server(upload_url, group_id, image_path):
@@ -88,9 +86,9 @@ if __name__ == '__main__':
     load_dotenv()
     access_token = os.getenv('ACCESS_TOKEN')
     group_id = 187000263
-    upload_url = get_upload_url(access_token,
+    upload_url = get_comics_upload_url(access_token,
                                 group_id)['response']['upload_url']
-    comics_comment, comics_name = get_comment_and_name()
+    comics_comment, comics_name = get_comics_comment_and_name()
     image_path = os.path.join(os.getcwd(), 'vk_comics', comics_name)
     upload_image_server = get_download_image_server(upload_url,
                                                     group_id, image_path)
